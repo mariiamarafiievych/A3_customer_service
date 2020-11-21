4afcc34bf6e74e9427bc24fa5b8e0140f784fb67
marafiievychmaria <mariamaraphiyevich@gmail.com>
2020-11-21 17:14:29 +0200

    New Commit

--- src/main/java/customer/controller/CustomerController.java
+++ src/main/java/customer/controller/CustomerController.java
@@ -14,7 +14,6 @@ import java.util.UUID;
 @RequestMapping("customers")
 public class CustomerController {
     private final CustomerService customerService;
-
     @Autowired
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
